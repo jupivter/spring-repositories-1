520fec1cc97cc9d08fdc6a9abf9f5c9195c7f50a
Marta Aramu <marta.aramu99@gmail.com>
2023-02-15 17:32:02 +0100

    Initial commit

---
+++ src/main/java/co/develhope/springrepositories1/entities/Car.java
@@ -0,0 +1,57 @@
+package co.develhope.springrepositories1.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "/cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String modelName;
+    private Long serialNumber;
+    private Float currentPrice;
+
+    public Car(){}
+
+    public Car (Integer id, String modelName, Long serialNumber, Float currentPrice) {
+        this.id = id;
+        this.modelName = modelName;
+        this.serialNumber = serialNumber;
+        this.currentPrice = currentPrice;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public Long getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(Long serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public Float getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(Float currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+}
